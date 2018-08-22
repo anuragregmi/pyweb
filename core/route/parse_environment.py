d7cf38fb64217dcb79714235c24e749f5246c7aa
Anurag Regmi <anurag.regmi@aayulogic.com>
2018-08-22 10:21:30 +0545

    Rename function parse_environ to parse_environment

--- core/route/parse_environment.py
+++ core/route/parse_environment.py
@@ -6,12 +6,13 @@ routes = {
 }
 
 
-def parse_environ(environment, start_response):
+def parse_environment(environment, start_response):
     headers = [("Content-Type", "text/html")]
     start_response("200 Ok", headers)
 
     path = environment.get('PATH_INFO', '/')
 
     response_string = routes.get(path, lambda x: "Hello World")(start_response)
+
     response = io.BytesIO(response_string.encode("utf-8"))
     return response
--- core/wsgi/application.py
+++ core/wsgi/application.py
@@ -1,6 +1,6 @@
-from ..route.parse_environment import parse_environ
+from ..route.parse_environment import parse_environment
 
 
 class Wsgi:
     def __call__(self, environment, start_response):
-        return parse_environ(environment, start_response)
+        return parse_environment(environment, start_response)
